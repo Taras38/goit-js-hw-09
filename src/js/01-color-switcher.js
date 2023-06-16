ae21263941c9b85c11a8c36858f1d3f22d1975cc
Taras38 <122573790+Taras38@users.noreply.github.com>
2023-06-16 15:42:53 +0300

    first-second task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+let intervalId;
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+startBtn.addEventListener("click", function () {
+    startBtn.disabled = true;  // disable the start button
+    intervalId = setInterval(function () {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+
+stopBtn.addEventListener("click", function () {
+    startBtn.disabled = false;  // enable the start button
+    clearInterval(intervalId);
+});
