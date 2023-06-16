078c6d99e839102687b98f8e0929773be33bf402
Taras38 <122573790+Taras38@users.noreply.github.com>
2023-06-16 12:43:33 +0000

    Deploying to gh-pages from @ Taras38/goit-js-hw-09@ae21263941c9b85c11a8c36858f1d3f22d1975cc 🚀

---
+++ 01-color-switcher.7b775a57.js
@@ -0,0 +1,2 @@
+let t;const e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");e.addEventListener("click",(function(){e.disabled=!0,t=setInterval((function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),n.addEventListener("click",(function(){e.disabled=!1,clearInterval(t)}));
+//# sourceMappingURL=01-color-switcher.7b775a57.js.map
